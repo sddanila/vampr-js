17abe4466e27107eda118b4cb3df008ff8ce937c
sddanila <sddanila@yahoo.co.uk>
2019-03-07 20:58:14 +0000

    add solutions to first Vampr exercise

--- vampire.js
+++ vampire.js
@@ -10,22 +10,29 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberOfVampires = 0;
+    let currentVampire = this;
+    while(currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return vampire.numberOfVampiresFromOriginal > this.numberOfVampiresFromOriginal;
   }
 
   /** Stretch **/
@@ -36,7 +43,18 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
+    let currentVampire = this;
+    console.log(currentVampire.name);
+    if (!currentVampire.creator) {
+      console.log(currentVampire.name);
+      return currentVampire.name;
+    }
+    if (!vampire.creator){
+      return vampire.name;
+    }
+    if (vampire.creator.name === currentVampire.name){
+      return vampire.creator.name;
+    }
   }
 }
 
